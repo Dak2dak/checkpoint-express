90aa91e852c23fa17e65cc2124a8a176fd196d23
Didier Dakoury <dazibao76@gmail.com>
2022-03-29 17:34:36 +0000

    react-app created

---
+++ Front-end/express-project/src/components/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HomePage = () => {
+
+    return (
+        <div>Home page</div>
+    )
+}
+
+/* Each page should contain a nav var link (home, Our services, Contact us)
+Feel free to put any content you want.
+The web application is only available during working hours (Monday to Friday,  from 9 to 17).*/
+
+export default HomePage;
+
+/*Use Express to create the server and handle routes.
+Create a custom middleware to verify the time of the request.
+The pages should be styled with CSS.
+Optionally, you can use any template engine you want.*/
---
+++ Front-end/express-project/src/components/OurServices.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const OurServices = () => {
+
+    return (
+        <div>Our services</div>
+    )
+}
+
+/* Each page should contain a nav var link (home, Our services, Contact us)
+Feel free to put any content you want.
+The web application is only available during working hours (Monday to Friday,  from 9 to 17).*/
+
+export default OurServices;
